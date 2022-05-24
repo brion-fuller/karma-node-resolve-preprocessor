887ab218d39dbd11bebd684145e4c9ea24b7d7fe
Julian Grinblat <julian@dotcore.co.il>
2022-05-24 22:44:32 +0900

    Add support for node module import algorithm

--- src/createNodeResolvePreprocessor.js
+++ src/createNodeResolvePreprocessor.js
@@ -1,5 +1,5 @@
 const { init, parse } = require("es-module-lexer");
-const { join } = require("path");
+const { join, dirname } = require("path");
 
 exports.isLocalPath = (filename) =>
   filename.startsWith("./") ||
@@ -18,34 +18,53 @@ exports.filePathToUrlPath = (filePath, basePath, urlRoot, proxyPath) => {
 };
 
 exports.createNodeResolvePreprocessor = (karmaConfig, logger) => {
+  let moduleResolve;
+  let findUp;
   const log = logger.create("preprocessor:node-resolve");
 
-  const resolvePath = (modulePath) => {
-    if (!exports.isLocalPath(modulePath)) {
+  const requirePackageJson = (packageName) => {
+    try {
+      return require(join(packageName, "package.json"));
+    } catch {}
+
+    return require(findUp("package.json", { cwd: dirname(require.resolve(packageName)) }));
+  }
+
+  const resolveAbsolutePath = (modulePath, parent) => {
+      try {
+        // First try to resolve using the official node algorithm
+        return moduleResolve(modulePath, `file://${parent}`, new Set(["browser", "default"])).pathname;
+      } catch (e) {
+        log.warn(`resolving ${modulePath} with the official node algorithm failed, ${e.message}. Attempting using unofficial resolve methods...`);
+      }
+
       const moduleMatch = modulePath.match(
         "((?:@[a-z0-9-~][a-z0-9-._~]*/)?[a-z0-9-~][a-z0-9-._~]*)(.+)?"
       );
       const packageName = moduleMatch[1];
       const path = moduleMatch[2];
-      let absolutePath = require.resolve(modulePath);
 
       // Check to make sure we are trying to pull in a module entry point
       if (!path) {
-        const { module, browser, type } = require(join(
-          packageName,
-          "package.json"
-        ));
+        const { module, browser, type } = requirePackageJson(packageName);
 
         if (module) {
-          absolutePath = require.resolve(join(packageName, module));
+          return require.resolve(join(packageName, module));
         } else if (browser) {
-          absolutePath = require.resolve(join(packageName, browser));
+          return require.resolve(join(packageName, browser));
         } else if (type !== "module") {
           log.warn(
             `${packageName} looks like a commonjs module.  This doesn't run natively in the browser.`
           );
         }
       }
+
+      return require.resolve(modulePath);
+  };
+
+  const resolvePath = (modulePath, parent) => {
+    if (!exports.isLocalPath(modulePath)) {
+      const absolutePath = resolveAbsolutePath(modulePath, parent);
 
       return exports.filePathToUrlPath(
         absolutePath,
@@ -59,6 +78,14 @@ exports.createNodeResolvePreprocessor = (karmaConfig, logger) => {
 
   return async (content, file, done) => {
     await init;
+    // esm-only modules
+    [
+      { moduleResolve },
+      { findUpSync: findUp },
+    ] = await Promise.all([
+        import("import-meta-resolve"),
+        import("find-up")
+      ]);
 
     log.debug('Processing "%s".', file.originalPath);
 
@@ -68,7 +95,7 @@ exports.createNodeResolvePreprocessor = (karmaConfig, logger) => {
 
       const patchedContent = imports.map(({ s, e }) => {
         const name = content.substring(s, e);
-        const resolved = resolvePath(name).replace(/\\/g, "\\\\");
+        const resolved = resolvePath(name, file.originalPath).replace(/\\/g, "\\\\");
         const text = content.slice(pointer, s) + resolved;
         pointer = e;
         return text;
